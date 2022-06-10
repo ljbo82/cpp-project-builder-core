b79ee2231b56c51512d262b22cfa9b32d852613d
Leandro Oliveira <leandro@agapsys.com>
2022-06-10 14:52:43 -0300

    Reverted changes

--- builder.mk
+++ builder.mk
@@ -622,7 +622,7 @@ pre-build: $(PRE_BUILD_DEPS)
         endif
     endif
 
---__builder_mk_build__: pre-build $(if $(SRC_FILES),$(O_BUILD_DIR)/$(ARTIFACT),)
+--__builder_mk_build__: $(if $(SRC_FILES),$(O_BUILD_DIR)/$(ARTIFACT),)
 
 --__builder_mk_post_build__: --__builder_mk_build__ $(POST_BUILD_DEPS)
 
@@ -630,7 +630,7 @@ define __builder_mk_build_target__
 .PHONY: build
 build: --__builder_mk_post_build__
 ifneq ($(SRC_FILES),)
-$(O_BUILD_DIR)/$(ARTIFACT): $(__builder_mk_obj_files__)
+$(O_BUILD_DIR)/$(ARTIFACT): pre-build $(__builder_mk_obj_files__)
     ifeq ($(PROJ_TYPE),lib)
         ifeq ($(LIB_TYPE),shared)
 	        @echo [LD] $$@
