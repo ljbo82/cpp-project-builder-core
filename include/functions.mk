4cd113f17ecd8b76c0a4e0c61295d8040e9b1c8e
Leandro Oliveira <ljbo82@users.noreply.github.com>
2024-02-17 17:28:39 +0000

    Multiple enhancements:
    - Improved comments
    - Added new functions
    - enhanced include guards

--- include/functions.mk
+++ include/functions.mk
@@ -21,12 +21,13 @@
 # General purpose functions
 
 ifndef cpb_include_functions_mk
-cpb_include_functions_mk := 1
+cpb_include_functions_mk := $(lastword $(MAKEFILE_LIST))
 
-override undefine cpb_include_functions_mk_token_prefix
+ifdef cpb_include_functions_mk_token_prefix
+    $(error [cpb_include_functions_mk_token_prefix] Reserved variable)
+endif
 
 cpb_include_functions_mk_token_prefix := __?__
-
 
 # Text functions ---------------------------------------------------------------
 
@@ -36,7 +37,7 @@ cpb_include_functions_mk_token_prefix := __?__
 # used to force split of empty tokens (afterwards token prefix shall be
 # removed).
 #
-# Syntax: $(call FN_SPLIT,baseString,delimiter,tokenPrefix=$(cpb_include_functions_mk_token_prefix)
+# Syntax: $(call FN_SPLIT,baseString,delimiter,tokenPrefix?=$(cpb_include_functions_mk_token_prefix)
 ifdef FN_SPLIT
     $(error [FN_SPLIT] Reserved variable)
 endif
@@ -76,7 +77,7 @@ FN_REVERSE = $(strip $(if $(1),$(call FN_REVERSE,$(wordlist 2,$(words $(1)),$(1)
 
 # Factorizes a host string (used to decompose host string into compatible layers).
 #
-# Syntax: $(call FN_HOST_FACTORIZE,factorized_string,[delimiter=-],[replacements=delimiter])
+# Syntax: $(call FN_HOST_FACTORIZE,factorized_string,delimiter?=-,replacements?=delimiter)
 ifdef FN_HOST_FACTORIZE
     $(error [FN_HOST_FACTORIZE] Reserved variable)
 endif
@@ -84,6 +85,18 @@ ifdef FN_HOST_FACTORIZE_previous
     $(error [FN_HOST_FACTORIZE_previous] Reserved variable)
 endif
 FN_HOST_FACTORIZE = $(foreach token,$(subst $(if $(2),$(2),-), ,$(1)),$(eval FN_HOST_FACTORIZE_previous=$(if $(FN_HOST_FACTORIZE_previous),$(FN_HOST_FACTORIZE_previous)$(if $(3),$(3),$(if $(2),$(2),-))$(token),$(token)))$(FN_HOST_FACTORIZE_previous))$(eval undefine FN_HOST_FACTORIZE_previous)
+
+# Numeric comparison of two numbers. Echoes the following output:
+#
+#  0: first == second
+#  1: first > second
+# -1: first < second
+#
+# Syntax: $(call FN_NUMBER_CMP,first?=0,second?=0)
+ifdef FN_NUMBER_CMP
+    $(error [FN_NUMBER_CMP] Reserved variable)
+endif
+FN_NUMBER_CMP = $(if $(call FN_EQ,$(if $(1),$(1),0),$(if $(2),$(2),0)),0,$(shell if [ $(if $(1),$(1),0) -gt $(if $(2),$(2),0) ]; then echo 1; else echo -1; fi))
 # ------------------------------------------------------------------------------
 
 # Semantic version functions ---------------------------------------------------
@@ -120,6 +133,34 @@ ifdef FN_SEMVER_PATCH
     $(error [FN_SEMVER_PATCH] Reserved variable)
 endif
 FN_SEMVER_PATCH = $(call FN_TOKEN,$(call FN_SEMVER_CHECK,$(1)),.,3)
+
+# Compares two semantic versions. Possible echoed values are:
+#
+# NOTE: patch component is ignored!
+#
+#  0: first == second
+# -2: Incompatible versions (firstVer.major < secondVer.major)
+#  2: Incompatible versions (firstVer.major > secondVer.major)
+# -1: firstVer.major == secondVer.major AND firstVer.minor < secondVer.minor
+#  1: firstVer.major == secondVer.major AND firstVer.minor > secondVer.minor
+#
+# Syntax: $(call FN_SEMVER_CMP,firstVer,secondVer)
+ifdef FN_SEMVER_CMP
+    $(error [FN_SEMVER_CMP] Reserved variable)
+endif
+ifdef FN_SEMVER_CMP_MAJOR
+    $(error [FN_SEMVER_CMP_MAJOR] Reserved variable)
+endif
+ifdef FN_SEMVER_CMP_MINOR
+    $(error [FN_SEMVER_CMP_MINOR] Reserved variable)
+endif
+FN_SEMVER_CMP = $(strip \
+$(eval FN_SEMVER_CMP_MAJOR := $(call FN_NUMBER_CMP,$(call FN_SEMVER_MAJOR,$(1)),$(call FN_SEMVER_MAJOR,$(2))))\
+$(eval FN_SEMVER_CMP_MINOR := $(call FN_NUMBER_CMP,$(call FN_SEMVER_MINOR,$(1)),$(call FN_SEMVER_MINOR,$(2))))\
+$(if $(call FN_EQ,$(FN_SEMVER_CMP_MAJOR),-1),-2,$(if $(call FN_EQ,$(FN_SEMVER_CMP_MAJOR),1),2,$(FN_SEMVER_CMP_MINOR)))\
+$(eval undefine FN_SEMVER_CMP_MAJOR)\
+$(eval undefine FN_SEMVER_CMP_MINOR)\
+)
 # ------------------------------------------------------------------------------
 
 # File system functions --------------------------------------------------------
@@ -150,42 +191,41 @@ FN_IS_INSIDE_DIR = $(filter $(abspath $(1)) $(abspath $(1)/%),$(abspath $(2)))
 # ------------------------------------------------------------------------------
 
 # Makefile utils ---------------------------------------------------------------
-
 # Checks if the origin of a variable matches with an expected value. If matching
 # fails, throws an error.
 #
-# Syntax $(call FN_CHECK_ORIGIN,varName,expectedOrigin[,errorMessage])
+# Syntax $(call FN_CHECK_ORIGIN,varName,expectedOrigin,errorMessage?=)
 ifdef FN_CHECK_ORIGIN
     $(error [FN_CHECK_ORIGIN] Reserved variable)
 endif
-FN_CHECK_ORIGIN = $(if $(call FN_EQ,$(origin $(1)),$(2)),,$(error [$(1)] $(if $(3),$(3),Unexpected origin: "$(origin $(1))" (expected: "$(2)"))))
+FN_CHECK_ORIGIN = $(if $(call FN_EQ,$(origin $(1)),$(2)),,$(error $(if $(3),$(3),[$(1)] Unexpected origin: "$(origin $(1))" (expected: "$(2)"))))
 
 # Checks for an unexpected/invalid words. If an invalid word is found,
 # throws an error.
 #
-# Syntax: $(call FN_CHECK_WORDS,varName,accepted_words[,errorMessage])
+# Syntax: $(call FN_CHECK_WORDS,varName,accepted_words,errorMessage?=)
 ifdef FN_CHECK_WORDS
     $(error [FN_CHECK_WORDS] Reserved variable)
 endif
-FN_CHECK_WORDS=$(if $(or $(word 2,$($(1))),$(filter-out $(2),$($(1)))),$(error [$(1)] $(if $(3),$(3),Invalid value: $($(1)))),)
+FN_CHECK_WORDS=$(if $(or $(word 2,$($(1))),$(filter-out $(2),$($(1)))),$(error $(if $(3),$(3),[$(1)] Invalid value: $($(1)))),)
 
 # Checks for a non-empty variable. If the value of $(varName) is empty, throws
 # an error.
 #
-# Syntax: $(call FN_CHECK_NON_EMPTY,varName[,errorMessage])
+# Syntax: $(call FN_CHECK_NON_EMPTY,varName,errorMessage?=)
 ifdef FN_CHECK_NON_EMPTY
     $(error [FN_CHECK_NON_EMPTY] Reserved variable)
 endif
-FN_CHECK_NON_EMPTY=$(if $(strip $($(1))),,$(error [$(1)] $(if $(2),$(2),Missing value)))
+FN_CHECK_NON_EMPTY=$(if $(strip $($(1))),,$(error $(if $(2),$(2),[$(1)] Missing value)))
 
 # Checks if whitespaces are found in an expected non-whitespace variable.
 # If a whitespace is found in $(varName), throws an error.
 #
-# Syntax: $(call FN_CHECK_NO_WHITESPACE,varName[,errorMessage])
+# Syntax: $(call FN_CHECK_NO_WHITESPACE,varName,errorMessage?=)
 ifdef FN_CHECK_NO_WHITESPACE
     $(error [FN_CHECK_NO_WHITESPACE] Reserved variable)
 endif
-FN_CHECK_NO_WHITESPACE=$(if $(call FN_EQ,0,$(words $($(1)))),,$(if $(call FN_EQ,1,$(words $($(1)))),,$(error [$(1)] $(if $(2),$(2),Value cannot have whitespaces: "$($(1))"))))
+FN_CHECK_NO_WHITESPACE=$(if $(call FN_EQ,0,$(words $($(1)))),,$(if $(call FN_EQ,1,$(words $($(1)))),,$(error $(if $(2),$(2),[$(1)] Value cannot have whitespaces: "$($(1))"))))
 # ------------------------------------------------------------------------------
 
 endif # ifndef cpb_include_functions_mk
