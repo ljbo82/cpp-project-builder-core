165869159a9b3e11184e571c7885da9488508099
Leandro Oliveira <ljbo82@users.noreply.github.com>
2024-05-01 14:57:39 +0100

    Simplified output

--- include/libs.mk
+++ include/libs.mk
@@ -101,7 +101,7 @@ PRE_BUILD_DEPS += $$(O_LIBS_DIR)/.$(1)
 # ==============================================================================
 .PHONY: --cpb-lib-$(1)
 --cpb-lib-$(1):
-	$$(call fn_log_cmd,$$(V),[LIB] $(4))
+	$$(call fn_log_cmd,$$(V),[LIB] $(1))
 	$$(V_PREFIX)$$(MAKE) $$(LIB_MKFLAGS_$(1))
 
 $$(O_LIBS_DIR)/.$(1): --cpb-lib-$(1) ;
