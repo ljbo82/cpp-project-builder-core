3ca308f154c6e9dd4c37a2fe49c3fbed7b9bb844
Leandro Oliveira <ljbo82@users.noreply.github.com>
2024-05-19 01:10:55 +0100

    Work-in-progress

--- fetcher.mk
+++ fetcher.mk
@@ -2,21 +2,13 @@ include $(CPB_DIR)/include/common.mk
 
 DOWNLOADS_DIR ?= $(O)/downloads
 
-# Syntax $(call cpb_fn_checksum_test_cmd,1:checksum,2:file)
-$(call fn_check_reserved,cpb_fn_checksum_test_cmd)
-cpb_fn_checksum_test_cmd = $(if $(findstring =,$(1)),$(call fn_token,$(1),=,1),md5)sum -c <<< '$(call fn_token,$(1),=,1) $(2)'
-
-# Syntax: $(call cpb_fn_download_cmd[https],1:uri,2:filename,[3:checksum])
+# Syntax: $(call cpb_fn_download_cmd[https],1:uri,2:filename)
 $(call fn_check_reserved,cpb_fn_download_cmd[https])
-cpb_fn_download_cmd[https] = wget $(1) $(strip $(if $(3),--no-check-certificate,) -O $(2))$(if $(3), && $(call cpb_fn_checksum_test_cmd,$(3),$(2)),) || rm $(2)
+cpb_fn_download_cmd[https] = wget $(1) $(strip $(if $(3),--no-check-certificate,) -O $(2))
 
-# Syntax: $(call cpb_fn_download_cmd[http],1:uri,2:filename,3:checksum)
+# Syntax: $(call cpb_fn_download_cmd[http],1:uri,2:filename)
 $(call fn_check_reserved,cpb_fn_download_cmd[http])
-cpb_fn_download_cmd[http] = wget $(1) -O $(2) && $(call cpb_fn_checksum_test_cmd,$(3),$(2)) || rm $(2)
-
-# Syntax: $(call cpb_fn_download_cmd[git],1:uri,2:dirname,3:reference)
-$(call fn_check_reserved,cpb_fn_download_cmd[git])
-cpb_fn_download_cmd[git] = (cd $(2)  > /dev/null || (git clone $(1) -o $(2) --recursive && cd $(2))) && git checkout $(3) && git clean -dfx
+cpb_fn_download_cmd[http] = wget $(1) -O $(2)
 
 $(call fn_check_reserved,SRC_URI[template])
 $(call fn_check_reserved,SRC_URI[uri])
@@ -57,8 +49,8 @@ ifeq ($$(SRC_URI[checksum]),)
     $$(call fn_warning,[WARNING][SRC_URI] Checksum not informed in URI entry '$(1)'!)
 else
     ifeq ($$(SRC_URI[scheme]),git)
-        $$(if $$(findstring =,$$(SRC_URI[checksum])),$$(call fn_warning,[WARNING][SRC_URI] Specified checksum type for a git entry ('$(1)'). It will be ignored!),)
-        SRC_URI[checksum_type] := git
+        $$(if $$(findstring =,$$(SRC_URI[checksum])),$$(call fn_error,[SRC_URI] Specified checksum type for a git entry ('$(1)'). It will be ignored!),)
+        SRC_URI[checksum_type] :=
     else
         SRC_URI[checksum_type] := $$(if $$(findstring =,$$(SRC_URI[checksum])),$$(call fn_token,$$(SRC_URI[checksum]),=,1),md5)
         ifeq ($$(call fn_test_patterns,md5 sha1 sha224 sha256 sha384 sha512,$$(SRC_URI[checksum_type])),)
@@ -97,59 +89,69 @@ ifeq ($$(SRC_URI[scheme]),git)
     endif
 endif
 
-# Download command associated with a file (will be used by recipe)
-SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd] := $$(call cpb_fn_download_cmd[$$(SRC_URI[scheme])],$$(SRC_URI[uri]),$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]),$$(SRC_URI[checksum]))
+# URI command associated with a file or dir
+SRC_URI[$$(SRC_URI[file_or_dir]).uri] := $$(SRC_URI[uri])
 
-# Checksum type associated with a file (will be used by recipe in non-git type)
-ifneq ($$(SRC_URI[type]),git)
+# Checksum associated with a file or dir
+SRC_URI[$$(SRC_URI[file_or_dir]).checksum] := $$(SRC_URI[checksum])
+
+# Checksum info associated with a file (non-git recipes)
+ifneq ($$(SRC_URI[scheme]),git)
     # Example expansion: SRC_URI[abc.tar.gz.checksum_type] := md5
     SRC_URI[$$(SRC_URI[file_or_dir]).checksum_type] := $$(SRC_URI[checksum_type])
-endif
 
-# Checksum associated with a file (will be used by recipe in non-git type)
-ifneq ($$(SRC_URI[type]),git)
     # Example expansion: SRC_URI[abc.tar.gz.md5.contents] := 0123456789abcdef abc.tar.gz
     SRC_URI[$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]).contents] := $$(SRC_URI[checksum]) $$(SRC_URI[file_or_dir])
+
+    # Download command associated with a file
+    SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd] := $$(call cpb_fn_download_cmd[$$(SRC_URI[scheme])],$$(SRC_URI[uri]),$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]))
 endif
 
-$$(info $(call fn_text,$(1),93))
-$$(info SRC_URI[uri]: $$(SRC_URI[uri]))
-$$(info SRC_URI[scheme]: $$(SRC_URI[scheme]))
-$$(info SRC_URI[checksum]: $$(SRC_URI[checksum]))
-$$(info SRC_URI[checksum_type]: $$(SRC_URI[checksum_type]))
-$$(info SRC_URI[file_or_dir]: $$(SRC_URI[file_or_dir]))
-$$(info SRC_URI[type]: $$(SRC_URI[type]))
-$$(info SRC_URI[$$(SRC_URI[file_or_dir]).checksum_type]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).checksum_type]))
-$$(info SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd]))
-$$(info SRC_URI[$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]).contents]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]).contents]))
-$$(info )
+# $$(info $(call fn_text,$(1),93))
+# $$(info SRC_URI[uri]: $$(SRC_URI[uri]))
+# $$(info SRC_URI[scheme]: $$(SRC_URI[scheme]))
+# $$(info SRC_URI[checksum]: $$(SRC_URI[checksum]))
+# $$(info SRC_URI[checksum_type]: $$(SRC_URI[checksum_type]))
+# $$(info SRC_URI[file_or_dir]: $$(SRC_URI[file_or_dir]))
+# $$(info SRC_URI[type]: $$(SRC_URI[type]))
+# $$(info SRC_URI[$$(SRC_URI[file_or_dir]).checksum_type]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).checksum_type]))
+# $$(info SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).download_cmd]))
+# $$(info SRC_URI[$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]).contents]: $$(SRC_URI[$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]).contents]))
+# $$(info )
 
 PRE_BUILD_DEPS := $$(PRE_BUILD_DEPS) $$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir])
 
-ifneq ($$(SRC_URI[type]),git)
 # ==============================================================================
-$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]): --force
-#   HINTS:
-#   1) Contents of $$@: $$(SRC_URI[$$(notdir $$@).contents]
-	$$(call fn_log,$$@,1)
-	$$(V_PREFIX)mkdir -p $$(dir $$@)
-    # Example expansion: [ "$(cat downloads/abc.tar.gz.md5 2> /dev/null)" = "0123456789abcdef abc.tar.gz" ] || echo "0123456789abcdef abc.tar.gz" > downloads/abc.tar.gz.md5
-	$$(V_PREFIX)[ "$$$$(cat $$@ 2> /dev/null)" = "$$(SRC_URI[$$(notdir $$@).contents])" ] || echo "$$(SRC_URI[$$(notdir $$@).contents])" > $$@
-# ==============================================================================
-endif
-
-# ==============================================================================
-$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]): $$(if $$(call fn_eq,$$(SRC_URI[type]),git),--force,$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]).$$(SRC_URI[checksum_type]))
+$$(DOWNLOADS_DIR)/$$(SRC_URI[file_or_dir]): --force
 #    HINTS:
-#    1) Checksum file associated with $$@ : $$@.$$(SRC_URI[$$(notdir $$@).checksum_type])
-#    2) Contents of checksum file associated with $$@: $$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])
-#    3) Download command associated with $$@: $$(SRC_URI[$$(notdir $$@).download_cmd])
-	@$$(call fn_log,$$@,1)
-    ifneq ($$(SRC_URI[type]),git)
-        # Example expansion: [ "$(cat downloads/abc.tar.gz.md5 2> /dev/null)" = "0123456789abcdef abc.tar.gz" ] || bash -c "wget...."
-	    $$(V_PREFIX)[ "$$$$(cat $$@.$$(SRC_URI[$$(notdir $$@).checksum_type]))" = "$$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])" ] || bash -c "$$(SRC_URI[$$(notdir $$@).download_cmd])"
+#    1) File associated with $$@: $$(notdir $$@)
+#    2) Checksum type associated with $$@: $$(SRC_URI[$$(notdir $$@).checksum_type])
+#    3) Checksum associated with $$@: $$(SRC_URI[$$(notdir $$@).checksum])
+#    4) Checksum file associated with $$@ : $$@.$$(SRC_URI[$$(notdir $$@).checksum_type])
+#    5) Contents of checksum file associated with $$@: $$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])
+#    6) Download command associated with $$@: $$(SRC_URI[$$(notdir $$@).download_cmd])
+#    7) URI associated with $$@: $$(SRC_URI[$$(notdir $$@).uri])
+
+    ifneq ($$(SRC_URI[scheme]),git)
+	    @mkdir -p $$(dir $$@)
+
+        # Updates checksum file...
+	    @$$(if $$(SRC_URI[$$(notdir $$@).checksum]),if [ "$$$$(cat $$@.$$(SRC_URI[$$(notdir $$@).checksum_type]) 2> /dev/null)" != "$$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])" ]; then echo "$$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])" > $$@.$$(SRC_URI[$$(notdir $$@).checksum_type]); fi,)
+
+        # Downloads only if file does not exist or checksum changed...
+	    @if [ ! -f $$@ ]$$(if $$(SRC_URI[$$(notdir $$@).checksum]), || [ "$$$$(cat $$@.$$(SRC_URI[$$(notdir $$@).checksum_type]))" != "$$(SRC_URI[$$(notdir $$@).$$(SRC_URI[$$(notdir $$@).checksum_type]).contents])" ],); then $$(call fn_log_cmd,[GET] $$(SRC_URI[$$(notdir $$@).uri]),$$(V)) && $$(SRC_URI[$$(notdir $$@).download_cmd]) || ($$(call fn_color_print_cmd,[SRC_URI] Download failure,91) && false); fi
+
+        # Check if downloaded file checksum matches...
+	    @$$(if $$(SRC_URI[$$(notdir $$@).checksum]),cd $$(dir $$@) && $$(subst .,,$$(suffix $$@.$$(SRC_URI[$$(notdir $$@).checksum_type])))sum -c $$(notdir $$@.$$(SRC_URI[$$(notdir $$@).checksum_type])) > /dev/null 2>&1 || ($$(call fn_color_print_cmd,[SRC_URI] Checksum failed for $$@,91) && false),)
+
+        # Extract
+        # TODO
     else
-	    $$(V_PREFIX)bash -c "$$(SRC_URI[$$(notdir $$@).download_cmd])"
+        # Clone repo only if directory does not exist...
+	    @if [ ! -d $$@ ]; then $$(call fn_log_cmd,[GIT] $$(SRC_URI[$$(notdir $$@).uri]),$$(V)) && git clone $$(SRC_URI[$$(notdir $$@).uri]) $$@ --recursive || ($$(call fn_color_print_cmd,[SRC_URI] Failure cloning repository,91) && false); fi
+
+        # Switching branches...
+	    @$$(if $$(SRC_URI[$$(notdir $$@).checksum]),@cd $$@ && git checkout -q $$(SRC_URI[$$(notdir $$@).checksum]) && git clean -dfx && git submodule update || ($$(call fn_color_print_cmd,[SRC_URI] Failure switching branches,91) && false),)
     endif
 # ==============================================================================
 # ------------------------------------------------------------------------------
